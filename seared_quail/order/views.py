a4fb3a2512d68076b7e69ce0242676111d03a9d5
Lucas Connors <lucas.revolutiontech@gmail.com>
2015-05-10 00:38:32 -0400

    Created initial kitchen interface

--- seared_quail/order/views.py
+++ seared_quail/order/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+"""
+:Created: 10 May 2015
+:Author: Lucas Connors
 
-# Create your views here.
+"""
+
+from django.views.generic import TemplateView
+
+from order.models import Order
+
+
+class KitchenView(TemplateView):
+
+    template_name = 'kitchen.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(KitchenView, self).get_context_data(**kwargs)
+        context['orders'] = Order.objects.all().order_by('id')
+        return context
--- seared_quail/seared_quail/urls.py
+++ seared_quail/seared_quail/urls.py
@@ -8,10 +8,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from menu.views import MenuView
+from order.views import KitchenView
 
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', MenuView.as_view(), name='menu'),
+    url(r'^kitchen/?$', KitchenView.as_view(), name='kitchen'),
 )
