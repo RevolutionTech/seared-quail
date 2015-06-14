1a8e51f97c44075ae5f42c567211097ad6b7563f
Lucas Connors <lucas.revolutiontech@gmail.com>
2015-06-14 18:49:52 -0400

    Bug fix: Do not allow admin user to set menu items as orderable but not viewable

--- seared_quail/menu/admin.py
+++ seared_quail/menu/admin.py
@@ -4,6 +4,7 @@
 
 """
 
+from django import forms
 from django.contrib import admin
 from ordered_model.admin import OrderedModelAdmin
 
@@ -14,7 +15,20 @@ class CategoryAdmin(OrderedModelAdmin):
     list_display = ('name', 'parent', 'move_up_down_links',)
 
 
+class MenuItemAdminForm(forms.ModelForm):
+    def clean(self):
+        cleaned_data = super(MenuItemAdminForm, self).clean()
+
+        if not self.errors:
+            # user_can_order and not show_in_menu is not allowed
+            if not cleaned_data['show_in_menu'] and cleaned_data['user_can_order']:
+                raise forms.ValidationError("User cannot order menu items not shown on the menu.")
+
+        return cleaned_data
+
+
 class MenuItemAdmin(OrderedModelAdmin):
+    form = MenuItemAdminForm
     list_display = ('name', 'category', 'show_in_menu', 'user_can_order', 'move_up_down_links',)
 
 
