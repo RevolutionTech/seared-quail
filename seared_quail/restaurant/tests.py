e9e1e6c3b6d228dbf2c4919d06935ebae4f67b5e
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-08-27 14:37:14 -0700

    FIX: Test method name should be get200s()

--- seared_quail/restaurant/tests.py
+++ seared_quail/restaurant/tests.py
@@ -9,10 +9,10 @@ from seared_quail.tests import SearedQuailTestCase
 
 class RestaurantAdminWebTestCase(SearedQuailTestCase):
 
-    def get200(self):
+    def get200s(self):
         return [
             '/admin/restaurant/',
             '/admin/restaurant/table/',
             '/admin/restaurant/table/add/',
-            '/admin/restaurant/table/{table_id}/change/'.format(table_id=self.table.id),
+            '/admin/restaurant/table/{table_id}/'.format(table_id=self.table.id),
         ]
