27df17c12864e03e8dc05c680a5cf598d0e3f04a
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-08-31 00:24:12 -0700

    Propagate warnings as errors when running tests

--- seared_quail/manage.py
+++ seared_quail/manage.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 import os
 import sys
+import warnings
+
+from django.utils.deprecation import RemovedInDjango20Warning
+
+# Propagate warnings as errors when running tests
+if len(sys.argv) >= 2 and sys.argv[1] == 'test':
+    warnings.filterwarnings('error')
+    # Filter warnings that are a result of dependencies
+    # They can be removed once the dependency fixes the issue
+    django20warnings = [
+        # django-ordered-model: https://github.com/bfirsh/django-ordered-model/issues/99
+        (
+            'Deprecated allow_tags attribute used on field move_up_down_links\. '
+            'Use django\.utils\.safestring\.format_html\(\), format_html_join\(\), or mark_safe\(\) instead\.'
+        ),
+    ]
+    for django20warning in django20warnings:
+        warnings.filterwarnings('ignore', category=RemovedInDjango20Warning, message=django20warning)
 
 import cbsettings
 
