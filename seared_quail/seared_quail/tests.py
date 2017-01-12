f8b8b47ea1dd078b5bc9750601fee65180db1413
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-01-11 22:48:22 -0800

    Use django-pigeon

--- seared_quail/seared_quail/tests.py
+++ seared_quail/seared_quail/tests.py
@@ -8,14 +8,16 @@ from django.apps import apps
 from django.contrib.auth.models import User
 from django.db import connection
 from django.db.migrations.executor import MigrationExecutor
-from django.test import TestCase, TransactionTestCase
+from django.test import TransactionTestCase
+
+from pigeon.test import RenderTestCase
 
 from menu.models import Category, MenuItem
 from order.models import Order, OrderMenuItem
 from restaurant.models import Table
 
 
-class SearedQuailTestCase(TestCase):
+class SearedQuailTestCase(RenderTestCase):
 
     USER_USERNAME = 'jsmith'
     USER_EMAIL = 'jsmith@example.com'
@@ -26,44 +28,6 @@ class SearedQuailTestCase(TestCase):
     MENU_ITEM_NAME = 'Water'
     MENU_ITEM_IN_SUBCATEGORY_NAME = 'Cola'
     TABLE_NUMBER = 'A-1'
-
-    @staticmethod
-    def strip_query_params(url):
-        return url.split('?')[0]
-
-    def assertResponseRenders(self, url, status_code=200, method='GET', data={}, has_form_error=False, **kwargs):
-        request_method = getattr(self.client, method.lower())
-        follow = status_code == 302
-        response = request_method(url, data=data, follow=follow, **kwargs)
-
-        if status_code == 302:
-            redirect_url, response_status_code = response.redirect_chain[0]
-        else:
-            response_status_code = response.status_code
-        self.assertEquals(
-            response_status_code,
-            status_code,
-            "URL {url} returned with status code {actual_status} when {expected_status} was expected.".format(
-                url=url,
-                actual_status=response_status_code,
-                expected_status=status_code
-            )
-        )
-
-        # Check that forms submitted did not return errors (or did if it should have)
-        form_error_assertion_method = self.assertIn if has_form_error else self.assertNotIn
-        form_error_assertion_method('errorlist', response.content)
-
-        return response
-
-    def assertResponseRedirects(self, url, redirect_url, method='GET', data={}, **kwargs):
-        response = self.assertResponseRenders(url, status_code=302, method=method, data=data, **kwargs)
-        redirect_url_from_response, _ = response.redirect_chain[0]
-        self.assertEquals(self.strip_query_params(redirect_url_from_response), redirect_url)
-        self.assertEquals(response.status_code, 200)
-
-    def get200s(self):
-        return []
 
     def setUp(self):
         super(SearedQuailTestCase, self).setUp()
@@ -88,10 +52,6 @@ class SearedQuailTestCase(TestCase):
         # Place an order
         self.order = Order.objects.create(table=self.table)
         OrderMenuItem.objects.create(order=self.order, menuitem=self.menu_item)
-
-    def testRender200s(self):
-        for url in self.get200s():
-            self.assertResponseRenders(url)
 
 
 class MigrationTestCase(TransactionTestCase):
