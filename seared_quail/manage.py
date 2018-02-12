266dd0d488233bb2f07d4e4d80d94786ef4df2ee
Lucas Connors <lucas.revolutiontech@gmail.com>
2018-02-11 16:42:43 -0800

    Do not propagate warnings as errors when running tests
    This is no longer relevant for Django 1.11+:
    https://docs.djangoproject.com/en/1.11/releases/1.11/#deprecating-warnings-are-no-longer-loud-by-default

--- seared_quail/manage.py
+++ seared_quail/manage.py
@@ -1,28 +1,6 @@
 #!/usr/bin/env python
 import os
 import sys
-import warnings
-
-from django.utils.deprecation import RemovedInDjango20Warning
-
-# Propagate warnings as errors when running tests
-if len(sys.argv) >= 2 and sys.argv[1] == 'test':
-    warnings.filterwarnings('error')
-    # Filter warnings that are a result of dependencies
-    # They can be removed once the dependency fixes the issue
-    django20warnings = [
-        # django-ordered-model: https://github.com/bfirsh/django-ordered-model/issues/99
-        (
-            'Deprecated allow_tags attribute used on field move_up_down_links\. '
-            'Use django\.utils\.html\.format_html\(\), format_html_join\(\), '
-            'or django\.utils\.safestring\.mark_safe\(\) instead\.'
-        ),
-
-        # django-ordered-model: https://github.com/bfirsh/django-ordered-model/issues/100
-        'Importing from django\.core\.urlresolvers is deprecated in favor of django\.urls\.',
-    ]
-    for django20warning in django20warnings:
-        warnings.filterwarnings('ignore', category=RemovedInDjango20Warning, message=django20warning)
 
 import cbsettings
 
