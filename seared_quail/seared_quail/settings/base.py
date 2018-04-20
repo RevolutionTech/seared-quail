c48515238ee7476611f4e2c68b01535ddbe1351a
Lucas Connors <lucas.revolutiontech@gmail.com>
2018-04-19 22:46:19 -0700

    Remove obsolete SessionAuthenticationMiddleware
    which serves no purpose in Django 1.10+

--- seared_quail/seared_quail/settings/base.py
+++ seared_quail/seared_quail/settings/base.py
@@ -35,7 +35,6 @@ class BaseSettings(DjangoDefaults):
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
     )
