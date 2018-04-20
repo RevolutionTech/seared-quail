488293cbcea2ccdc110bca8c0c8499130b25569d
Lucas Connors <lucas.revolutiontech@gmail.com>
2018-04-19 22:31:18 -0700

    Import reverse() from django.urls
    instead of deprecated django.core.urlresolvers

--- seared_quail/menu/views.py
+++ seared_quail/menu/views.py
@@ -5,7 +5,7 @@
 """
 
 from django.contrib import messages
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.views.generic.edit import FormView
 
 from menu.forms import MenuForm
